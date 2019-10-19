469ba4ca751cca95b1ff3130bc18ec798e1bc17e
Leonardo <leonardohenriquedemelo@outlook.com>
2019-10-19 13:47:36 -0300

    Criação da Janela Principal Básica e seus Listeners.

--- AgroTracker/src/agrotracker/AgroTracker.java
+++ AgroTracker/src/agrotracker/AgroTracker.java
@@ -4,18 +4,73 @@
  * and open the template in the editor.
  */
 package agrotracker;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLFrameHyperlinkEvent;
 
-/**
- *
- * @author Unifei
- */
-public class AgroTracker {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class Hyperactive implements HyperlinkListener {
+    
+    private JEditorPane editorPane;
+    
+    public Hyperactive(JEditorPane editorPane){
+        this.editorPane = editorPane;
     }
     
+    @Override
+    public void hyperlinkUpdate(HyperlinkEvent e) 
+    { 
+        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) 
+        { 
+            HyperlinkEvent.EventType type = e.getEventType();
+            final URL url = e.getURL();
+            if (type == HyperlinkEvent.EventType.ENTERED) {
+                System.out.println("URL: " + url);
+            } else if (type == HyperlinkEvent.EventType.ACTIVATED) {
+            System.out.println("Activated");
+
+            Document doc = editorPane.getDocument();
+                try {
+                    editorPane.setPage(url);
+                } catch (IOException ex) {
+                    Logger.getLogger(Hyperactive.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }    
+        } 
+    } 
 }
+
+public class AgroTracker{
+
+	public AgroTracker() throws IOException{
+		JFrame frame = new JFrame("Agro Tracker");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		try {
+                    JEditorPane editorPane = new JEditorPane("file:ArquivosHTML/loginPage.html");
+                    editorPane.setEditable(false);
+
+                    Hyperactive hyperlinkListener = new Hyperactive(editorPane);
+                          editorPane.addHyperlinkListener(hyperlinkListener);
+                          JScrollPane scrollPane = new JScrollPane(editorPane);
+                          frame.add(scrollPane);
+                } catch (IOException e) {
+                          System.err.println("Unable to load: " + e);
+                }
+
+		frame.setBounds(50, 50, 600, 500);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) throws IOException{
+		AgroTracker iniciaSistema = new AgroTracker();
+	}
+}
+   
