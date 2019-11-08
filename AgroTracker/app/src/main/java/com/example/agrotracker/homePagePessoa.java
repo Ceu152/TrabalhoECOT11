2f587de299cfc6f3979c5913b3cd17cd7433ac87
Leonardo <leonardohenriquedemelo@Outlook.com>
2019-11-07 23:45:47 -0300

    Criação de 3 activitys
    Criação de mais 3 activitys

---
+++ AgroTracker/app/src/main/java/com/example/agrotracker/homePagePessoa.java
@@ -0,0 +1,15 @@
+package com.example.agrotracker;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.agrotracker.R;
+
+public class homePagePessoa extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_pessoa);
+    }
+}
---
+++ AgroTracker/app/src/main/java/com/example/agrotracker/homePageProdutor.java
@@ -0,0 +1,12 @@
+package com.example.agrotracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class homePageProdutor extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_produtor);
+    }
+}
